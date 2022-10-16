a566d122961d97d5c06426e0417cd3771c6e0ad2
kazuyahirotsu <hrtkzy4444@gmail.com>
2022-10-16 22:59:47 +0900

    Deleted crossdirection drawn with line

--- dev/src/DrawCanvas/handlers/line.js
+++ dev/src/DrawCanvas/handlers/line.js
@@ -34,7 +34,6 @@ line.onMouseUp = function onMouseUp(position, callback) {
         const data = [[this.state.firstMouseDown.x, this.state.firstMouseDown.y], [position.x, position.y]];
         const start = this.state.start;
         const options = this.state.options;
-        this.drawCrossDirection(this.state.data, 10);
         this.resetState();
         callback();
         return {
@@ -46,37 +45,6 @@ line.onMouseUp = function onMouseUp(position, callback) {
             }
         };
     }
-}
-
-function getCrossPath(point, size, direction) {
-    const path = new Path2D();
-    const startHorizontalLine = { x: point.x - size, y: point.y };
-    const endHorizontalLine = { x: point.x + size, y: point.y };
-    const startVerticalLine = { x: point.x, y: point.y - size };
-    const endVerticalLine = { x: point.x, y: point.y + size };
-
-    path.moveTo(startHorizontalLine.x, startHorizontalLine.y);
-    path.lineTo(endHorizontalLine.x, endHorizontalLine.y);
-    path.moveTo(startVerticalLine.x, startVerticalLine.y);
-    path.lineTo(endVerticalLine.x, endVerticalLine.y);
-    return path;
-}
-
-/* Xt = (X1+X2)/2 + M * sign(Y2-Y1)
-Yt = (Y1+Y2)/2 - M * sign(X2-X1) */
-line.drawCrossDirection = function (points, pixelDistance) {
-    const x1 = points[0][0];
-    const x2 = points[1][0];
-    const y1 = points[0][1];
-    const y2 = points[1][1];
-
-    const xCoord = ((x1 + x2) / 2) + (pixelDistance * Math.sign(y2 - y1));
-    const yCoord = ((y1 + y2) / 2) - (pixelDistance * Math.sign(x2 - x1));
-
-    const crossPath = getCrossPath({x: xCoord, y: yCoord }, 6);
-    this.ctx.strokeStyle = '#ff0000';
-    this.ctx.stroke(crossPath);
-    this.ctx.strokeStyle = '#000';
 }
 
 export default line;
